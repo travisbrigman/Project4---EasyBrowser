087d63129367e27e0f78871ff21749a51a0228b1
travisbrigman <travis1000@gmail.com>
2021-05-09 06:39:30 -0500

    completed with challenges

---
+++ Project4 - EasyBrowser/TableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  TableViewController.swift
+//  Project4 - EasyBrowser
+//
+//  Created by Travis Brigman on 5/9/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var websites = ["apple.com", "hackingwithswift.com"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "website", for: indexPath)
+        cell.textLabel?.text = websites[indexPath.row]
+        return cell
+    }
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "webView") as? ViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedSite = websites[indexPath.row]
+
+
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project4 - EasyBrowser/ViewController.swift
+++ Project4 - EasyBrowser/ViewController.swift
@@ -11,7 +11,8 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView = WKWebView()
     var progressView = UIProgressView()
-    var websites = ["apple.com", "hackingwithswift.com"]
+//    var websites = ["apple.com", "hackingwithswift.com"]
+    var selectedSite: String?
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
@@ -27,8 +28,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://" + websites[0])!
-        webView.load(URLRequest(url: url))
+//        let url = URL(string: "https://" + websites[0])!
+        if let selectedSite = selectedSite {
+            let url = URL(string: "https://" + selectedSite)!
+            webView.load(URLRequest(url: url))
+        }
         webView.allowsBackForwardNavigationGestures = true
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
@@ -48,9 +52,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
-        for site in websites {
-            ac.addAction(UIAlertAction(title: site, style: .default, handler: openPage))
-        }
+//        for site in websites {
+            ac.addAction(UIAlertAction(title: selectedSite, style: .default, handler: openPage))
+//        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -60,11 +64,13 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = navigationAction.request.url
         
         if let host = url?.host {
-            for website in websites {
-                if host.contains(website) {
+            if let selectedSite = selectedSite {
+//            for website in websites {
+                if host.contains(selectedSite) {
                     decisionHandler(.allow)
                     return
                 }
+//            }
             }
             let invalid = UIAlertController(title: "invalid site", message: "navigation to site isn't allowed", preferredStyle: .alert)
             invalid.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: .none))
